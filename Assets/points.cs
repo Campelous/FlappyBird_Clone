b3919f4ae8056bd7d7699de5779505b67a368f4e
Sergio Campelo <sergioocf10@gmail.com>
2022-07-07 23:49:20 -0300

    agora foi

---
+++ Assets/points.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class points : MonoBehaviour
+{
+
+    public GameController controller;
+
+    void Start(){
+        controller = FindObjectOfType<GameController>();
+    }
+    
+    void OnTriggerEnter2D(Collider2D colisor){
+
+        controller.Score++;
+        controller.scoreText.text = controller.Score.ToString();
+
+    }
+
+}
